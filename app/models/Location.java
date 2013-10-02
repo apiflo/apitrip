4da02aae928b9e5568169591baf0a23738afee05
apiflo <florian.mousseau@gmail.com>
2013-10-02 19:17:27 -0400

    direction 1;

---
+++ app/models/Location.java
@@ -0,0 +1,39 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+@Entity
+public class Location extends Model {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+
+	public String title;
+	
+	public Float latitude;
+	
+	public Float longitude;
+
+	public String descripion;
+
+	public static Finder<Long, Location> find = new Finder<Long, Location>(
+			Long.class, Location.class);
+
+	public static void create(Location location) {
+		location.save();
+	}
+
+	public static void delete(Long id) {
+		find.ref(id).delete();
+	}
+
+	public static void update(Location location) {
+		location.save();
+	}
+
+}
